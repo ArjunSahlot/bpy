2e2b50450ef22391237c78f3c9adbe82a73abf46
ArjunSahlot <iarjun.sahlot@gmail.com>
2020-12-16 20:28:23 -0800

    added context

--- fake_bpy/__init__.py
+++ fake_bpy/__init__.py
@@ -1,1 +1,9 @@
-a = 1
+from . import context
+# from . import data
+# from . import msgbus
+# from . import ops
+# from . import types
+# from . import utils
+# from . import path
+# from . import app
+# from . import props
