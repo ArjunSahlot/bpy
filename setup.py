2832ec3fb5350d532da0c5f68c095209160f497f
ArjunSahlot <iarjun.sahlot@gmail.com>
2020-12-16 20:06:34 -0800

    initial release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="module",
+    name="fake_bpy",
     version="0.0.1",
     author="Arjun Sahlot",
     author_email="iarjun.sahlot@gmail.com",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ArjunSahlot/",
-    py_modules=["module"],
+    py_modules=["fake_bpy"],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
