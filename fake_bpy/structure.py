be016e7995e6a1bf1dd073778bfea663e254588e
ArjunSahlot <iarjun.sahlot@gmail.com>
2020-12-17 13:57:11 -0800

    Added __init__ note

--- fake_bpy/structure.py
+++ fake_bpy/structure.py
@@ -2,7 +2,7 @@ import random
 
 
 class bpy_struct:
-    id_data = 
+    id_data = ID()
     def as_pointer(self):
         return random.randint(100, 999999)
 
@@ -40,4 +40,15 @@ class bpy_struct:
         return self.__dict__.keys()
     
     def path_from_id(self, property=""):
-        return self.id_data
+        return self.id_data.name
+
+    def path_resolve(self, path, coerce=True):
+        raise ValueError(f"Property not found from the path {path}")
+
+    def pop(self, key, default=None):
+        return default
+    
+    def property_overridable_library_set(self, property, overridable):
+        return False
+    
+    def property_unset()
