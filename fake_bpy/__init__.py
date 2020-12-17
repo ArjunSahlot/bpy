a92e14791d272565ba8b4cfed0857225979fcc02
ArjunSahlot <iarjun.sahlot@gmail.com>
2020-12-16 23:10:21 -0800

    Finished

--- fake_bpy/__init__.py
+++ fake_bpy/__init__.py
@@ -1,9 +1,11 @@
-from . import context
-# from . import data
-from . import msgbus
-# from . import ops
-# from . import types
-# from . import utils
-# from . import path
-# from . import app
-# from . import props
+from ._utils import _NoneType
+
+context = _NoneType()
+data = _NoneType()
+msgbus = _NoneType()
+ops = _NoneType()
+types = _NoneType()
+utils = _NoneType()
+path = _NoneType()
+app = _NoneType()
+props = _NoneType()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fake_bpy",
-    version="0.0.4",
+    version="0.0.6",
     author="Arjun Sahlot",
     author_email="iarjun.sahlot@gmail.com",
     description="Module with various useful utilities.",
